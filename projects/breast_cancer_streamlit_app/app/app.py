5df21691d829a25bc36f91a2f5c3104e77297904
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-06-01 19:13:40 +0100

    made an update

--- projects/breast_cancer_streamlit_app/app/app.py
+++ projects/breast_cancer_streamlit_app/app/app.py
@@ -49,7 +49,7 @@ def add_sidebar():
     input_dict = {}
     
     for label, key in slider_labels:
-        st.sidebar.slider(
+        input_dict[key] = st.sidebar.slider(
             label,
             min_value=float(0),
             max_value=float(data[key].max()),
@@ -58,29 +58,51 @@ def add_sidebar():
     return input_dict  
 
 def get_radar_chart(input_data):
-    categories = ['processing cost','mechanical properties','chemical stability',
-              'thermal stability', 'device integration']
-
+    categories = ['Radius', 'Texture', 'Perimeter',
+                  'Smoothness', 'Compactness',
+                  'Concavity', 'Concave Points',
+                  'Symmetry', 'Fractal Dimension']
     fig = go.Figure()
 
     fig.add_trace(go.Scatterpolar(
-          r=[1, 5, 2, 2, 3],
+        r=[
+            input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
+            input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
+            input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
+            input_data['fractal_dimension_mean']
+        ],
           theta=categories,
           fill='toself',
-          name='Product A'
+          name='Mean Value'
     ))
     fig.add_trace(go.Scatterpolar(
-          r=[4, 3, 2.5, 1, 2],
+        r=[
+            input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'],
+            input_data['area_se'], input_data['smoothness_se'], input_data['compactness_se'],
+            input_data['concavity_se'], input_data['concave points_se'], input_data['symmetry_se'],
+            input_data['fractal_dimension_se']
+        ],
           theta=categories,
           fill='toself',
-          name='Product B'
+          name='Standard Error'
+    ))
+    fig.add_trace(go.Scatterpolar(
+        r=[
+            input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
+            input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
+            input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
+            input_data['fractal_dimension_worst']
+        ],
+          theta=categories,
+          fill='toself',
+          name='Worst Value'
     ))
 
     fig.update_layout(
       polar=dict(
         radialaxis=dict(
           visible=True,
-          range=[0, 5]
+          range=[0, 1]
         )),
       showlegend=True
     )
