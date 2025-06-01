20d82835046542cb325bb1881ca4d01f1679980a
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-06-01 18:19:55 +0100

    made an update

--- projects/breast_cancer_streamlit_app/app/app.py
+++ projects/breast_cancer_streamlit_app/app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from joblib import dump, load
+import plotly.graph_objects as go
 
 def get_clean_data():
     data = pd.read_csv("../data/data.csv")    
@@ -56,6 +57,35 @@ def add_sidebar():
         )
     return input_dict  
 
+def get_radar_chart(input_data):
+    categories = ['processing cost','mechanical properties','chemical stability',
+              'thermal stability', 'device integration']
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+          r=[1, 5, 2, 2, 3],
+          theta=categories,
+          fill='toself',
+          name='Product A'
+    ))
+    fig.add_trace(go.Scatterpolar(
+          r=[4, 3, 2.5, 1, 2],
+          theta=categories,
+          fill='toself',
+          name='Product B'
+    ))
+
+    fig.update_layout(
+      polar=dict(
+        radialaxis=dict(
+          visible=True,
+          range=[0, 5]
+        )),
+      showlegend=True
+    )
+    
+    return fig
 
 def main():
     st.set_page_config(
@@ -74,7 +104,8 @@ def main():
     col1, col2 = st.columns([4,1])
     
     with col1:
-        st.write("this is column 1")
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
     with col2:
         st.write("this is columns 2")
         
