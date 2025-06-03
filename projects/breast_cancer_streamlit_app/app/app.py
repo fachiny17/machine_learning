1c85941326aa3c7bd13c1bb255ea95fb70cd51a7
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-06-03 20:51:54 +0100

    made an update

--- projects/breast_cancer_streamlit_app/app/app.py
+++ projects/breast_cancer_streamlit_app/app/app.py
@@ -147,7 +147,7 @@ def add_predictions(input_data):
         
     st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
     st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
-
+    st.write("Thi app can assist medical professionals in making a diagnosis, but should not be used as a substitute for professional diagnosis.")
         
 def main():
     st.set_page_config(
